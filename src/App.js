f60e10717f5b79f8e510afcb18638c54303efe05
chandru <717822p307@kce.ac.in>
2025-07-03 10:38:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import FeedbackForm from "./components/feedback";
+import ReviewList from "./components/review";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <h1>🎬 Movie Feedback App</h1>
+        <div className="tab">
+          <Link to="/feedback" className="tab-link">Give Feedback</Link>
+          <Link to="/reviews" className="tab-link">View Reviews</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/feedback" />} />
+          <Route path="/feedback" element={<FeedbackForm />} />
+          <Route path="/reviews" element={<ReviewList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
